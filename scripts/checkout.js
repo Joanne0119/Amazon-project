aed1462ce0847800c08dd0ed97ddde20a224c25e
JoanneLiu <liuchengen52033@gmail.com>
2023-11-28 15:13:34 +0800

    import esm libraries into checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,9 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import {hello} from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+//defual export
 
 hello();
 const today = dayjs();
